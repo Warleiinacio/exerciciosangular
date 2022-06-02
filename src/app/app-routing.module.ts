009c6da76e2233cba752e0fc31388d878053a054
warley inacio <warleiinacio66@gmail.com>
2022-06-02 01:45:49 -0300

    usando bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { MangaComponent } from './component/sobre/manga/manga.component';
+import { ImagensComponent } from './component/sobre/imagens/imagens.component';
+import { HistoriaComponent } from './component/sobre/historia/historia.component';
+
+import { MainComponent } from './components/main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'sobre/historia', component: HistoriaComponent},
+  {path: 'sobre/imagens', component: ImagensComponent},
+  {path: 'sobre/manga', component: MangaComponent},
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ImagensComponent } from './component/sobre/imagens/imagens.component';
+import { MangaComponent } from './component/sobre/manga/manga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ImagensComponent,
+    MangaComponent
   ],
   imports: [
     BrowserModule,
